2c59d3c6a1141321173e7a5ec2dd584ecc0490f1
Daikus007 <moyafabian989@gmail.com>
2024-09-16 21:45:36 -0500

    Subir nuestro proyecto a GitHub

---
+++ src/main/java/AppLogistica/com/AppLogistica/AppLogisticaApplication.java
@@ -0,0 +1,13 @@
+package AppLogistica.com.AppLogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLogisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AppLogistica/com/AppLogistica/AppLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package AppLogistica.com.AppLogistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
